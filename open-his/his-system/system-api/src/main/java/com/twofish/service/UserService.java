6d1ea236b2ce98d7e4cf635c137f0d59ed9cc570
yinuan-i <903292861@qq.com>
2020-11-09 11:45:27 +0800

    用户模块提交

---
+++ open-his/his-system/system-api/src/main/java/com/twofish/service/UserService.java
@@ -0,0 +1,53 @@
+package com.twofish.service;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.twofish.domain.User;
+
+/**
+ * @author Ytyy
+ * @version 1.0
+ */
+public interface UserService {
+
+    /**
+     * 根据手机号查询用户
+     *
+     * @param phone 用户手机号
+     * @return 单个用户实体
+     */
+    User querybyphone(String phone);
+
+    /**
+     * 根据用户id查询用户
+     *
+     * @param userId 用户id
+     * @return 单个用户实体
+     */
+    User getOne(Long userId);
+
+    /**
+     * 分页查询用户
+     *
+     * @param page 分页实体类
+     * @param wrapper 条件查询
+     * @return 用户列表
+     */
+    Page<User> getUserByPage(Page<User> page, Wrapper<User> wrapper);
+
+    /**
+     * 添加用户
+     *
+     * @param user 用户实体
+     * @return 受影响的行数
+     */
+    Integer addUser(User user);
+
+    /**
+     * 根据id删除用户
+     *
+     * @param userId 用户id
+     * @return 受影响的行数
+     */
+    Integer deleteUserById(Long userId);
+}
---
+++ open-his/his-system/system-service/src/main/java/com/twofish/service/impl/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.twofish.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.twofish.domain.User;
+import com.twofish.mapper.UserMapper;
+import com.twofish.service.UserService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Ytyy
+ * @version 1.0
+ */
+@Service
+public class UserServiceImpl implements UserService {
+    @Resource
+    private UserMapper userMapper;
+
+    @Override
+    public User querybyphone(String phone) {
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.eq(User.COL_PHONE, phone);
+        return userMapper.selectOne(wrapper);
+    }
+
+    @Override
+    public User getOne(Long userId) {
+        return userMapper.selectById(userId);
+    }
+
+    @Override
+    public Page<User> getUserByPage(Page<User> page, Wrapper<User> wrapper) {
+        return userMapper.getUserAndDeptNamePage(page, wrapper);
+    }
+
+    @Override
+    public Integer addUser(User user) {
+        return userMapper.insert(user);
+    }
+
+    @Override
+    public Integer deleteUserById(Long userId) {
+        return userMapper.deleteById(userId);
+    }
+}
